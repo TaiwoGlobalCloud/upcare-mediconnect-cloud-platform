e6f98033c08314e465daf4990d242ab8f66f409e
Taiwo Justice <taiwojusticeo@gmail.com>
2026-08-03 22:29:36 -0500

    Fix recursive CloudTrail module

--- terraform/modules/cloudtrail/main.tf
+++ terraform/modules/cloudtrail/main.tf
@@ -1,20 +1,14 @@
 #############################################
-# CloudTrail Module
+# AWS CloudTrail Module
+# UpCare MediConnect Cloud Platform
 #############################################
 
-module "cloudtrail" {
-
-  source = "./modules/cloudtrail"
-
-  project_name = var.project_name
-  environment  = var.environment
-
-  trail_name                 = var.trail_name
-  enable_log_file_validation = var.enable_log_file_validation
-  is_multi_region_trail      = var.is_multi_region_trail
-
-  s3_bucket_name = module.s3.bucket_ids["audit"]
-
-  kms_key_arn = module.kms.key_arn
-
-}
+# This module's resources are organized into:
+#
+# - trail.tf
+# - policies.tf
+# - data.tf
+# - variables.tf
+# - outputs.tf
+#
+# No resources are defined in this file.
